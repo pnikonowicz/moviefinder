cf48472a81363da0a9e0cdaa6d66544ddbb9bde7
Paul <paul@gratafy.com>
2016-11-18 22:05:11 -0800

    blah

--- app/src/main/java/com/example/paulnikonowicz/zocdocapplication/MainActivity.java
+++ app/src/main/java/com/example/paulnikonowicz/zocdocapplication/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.paulnikonowicz.zocdocapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
     }
 }
